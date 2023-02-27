0c7f673fe1a8b7ab8e78f84e93ff724b326d50da
Christophe Loeys <christophe.loeys@allphi.eu>
2023-02-27 16:36:41 +0100

    more bird stuff

---
+++ Globals/Enums/Enums.gd
@@ -0,0 +1,6 @@
+class_name Enums
+extends RefCounted
+
+enum NestType {CAVITY, PLATFORM, BOWL, GROUND, ANY, NONE}
+enum Habitat {FOREST, GRASSLANDS, WETLANDS}
+enum FoodType {RAT, FRUIT, INSECT, GRAIN, FISH, ANY}
